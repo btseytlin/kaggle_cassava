6d0cc40722f4222577a29566543109b9520b2038
Boris <b.tseytlin@lambda-it.ru>
2020-12-08 14:20:38 +0300

    New image augs, UMAP visualization

--- src/cassava/transforms.py
+++ src/cassava/transforms.py
@@ -6,9 +6,9 @@ from torchvision import transforms
 
 def get_train_transforms():
     return A.Compose([
+        A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=5, val_shift_limit=5, p=1),
         A.ToFloat(max_value=1.0),
-        A.Resize(256, 256),
-        A.RandomResizedCrop(227, 227, scale=(0.4, 1), ratio=(0.75, 1.33)),
+        A.RandomResizedCrop(256, 256, scale=(0.3, 0.9)),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -19,8 +19,8 @@ def get_train_transforms():
 def get_test_transforms():
     return A.Compose([
         A.ToFloat(max_value=1.0),
-        A.Resize(256, 256),
-        A.CenterCrop(227, 227),
+        A.Resize(400, 400),
+        A.CenterCrop(256, 256),
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ToTensorV2(),
     ])
